9eccad3c6e4af0f4f3edeacdce06f228538fd52b
Pratiksha <pratu@gmail.com>
2024-11-14 22:51:18 +0530

    All navbar pages created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*index.html","./verify.html"],
+  content: ["./*index.html","./*verify.html","./*about.html",
+    "./*contact.html","./*Apply.html"],
   theme: {
     extend: {},
   },
